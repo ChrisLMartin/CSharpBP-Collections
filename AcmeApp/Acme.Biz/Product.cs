c3b2b2c7ac7e573fb7cb69f33a9948c160033a62
ChrisLMartin <christopherlancemartin@gmail.com>
2018-09-14 09:03:33 +1000

    Up to generic lists

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -16,6 +16,30 @@ namespace Acme.Biz
         #region Constructors
         public Product()
         {
+            //var colorOptions = new string[4];
+            //colorOptions[0] = "Red";
+            //colorOptions[1] = "Espresso";
+            //colorOptions[2] = "White";
+            //colorOptions[3] = "Navy";
+
+            string[] colorOptions = { "Red", "Espresso", "White", "Navy" };
+            
+            var brownindex = Array.IndexOf(colorOptions, "Espresso");
+
+            colorOptions.SetValue("Blue", 3);
+            
+            for (int i = 0; i < colorOptions.Length; i++)
+            {
+                colorOptions[i] = colorOptions[i].ToLower();
+                Console.WriteLine($"The color is {colorOptions[i]}");
+            }
+
+            foreach (var color in colorOptions)
+            {
+                Console.WriteLine($"The color is {color}");
+            }
+
+            Console.WriteLine(colorOptions);
         }
         public Product(int productId,
                         string productName,
@@ -84,8 +108,25 @@ namespace Acme.Biz
         /// </summary>
         /// <param name="markupPercent">Percent used to mark up the cost.</param>
         /// <returns></returns>
-        public decimal CalculateSuggestedPrice(decimal markupPercent) =>
-             this.Cost + (this.Cost * markupPercent / 100);
+        public OperationResult<decimal> CalculateSuggestedPrice(decimal markupPercent)
+        {
+            var message = "";
+
+            if (markupPercent <= 0m)
+            {
+                message = "Invalid markup percentage";
+            }
+            else if (markupPercent < 10)
+            {
+                message = "Below recommended markup percentage";
+            }
+            var value = this.Cost + (this.Cost * markupPercent / 100);
+
+            var operationResult = new OperationResult<decimal>(value, message);
+            return operationResult;
+
+        }
+             
 
         public override string ToString()
         {
--- AcmeApp/Acme.Biz/VendorRepository.cs
+++ AcmeApp/Acme.Biz/VendorRepository.cs
@@ -29,6 +29,26 @@ namespace Acme.Biz
             return vendor;
         }
 
+        public T RetreiveValue<T>(string sql, T defaultValue) // where T : struct
+        {
+            // Call the database to retrieve the value
+            // If no value is returned, return the default value
+            T value = defaultValue;
+
+            return value;
+        }
+
+
+        // Overload required due to generic constraint limitations
+        //public string RetreiveValue<T>(string sql, string defaultValue)
+        //{
+        //    // Call the database to retrieve the value
+        //    // If no value is returned, return the default value
+        //    string value = defaultValue;
+
+        //    return value;
+        //}
+
         /// <summary>
         /// Save data for one vendor.
         /// </summary>
